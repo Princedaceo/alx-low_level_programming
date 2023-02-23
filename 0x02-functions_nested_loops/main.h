163ccec04fa28825d9fd607149fa940c9ccbad64
Princedaceo <princedaceo1@gmail.com>
2023-02-22 20:52:45 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,11 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _alpha(int c);
 int print_sign(int n);
+int -abs(int);
+int print_last_digit(int);
+void jaxk_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
